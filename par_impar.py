e079fc4fafd0edeec2d8bb466660be9313fb8dab
Nicolas942 <nicolascabrera@colegioguanenta.edu.co>
2025-01-31 10:01:39 -0500

    Actualización

--- par_impar.py
+++ par_impar.py
@@ -3,5 +3,5 @@ num = int(input("Ingrese un numero para determinar si es par o impar "))
 if num % 2 == 0:
     print(f"EL número {num} es par")
 else:
-    print(f"El número {num} es impar")
+    print(f"El número {num} es impar ")
 
